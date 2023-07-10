1040b0d18837568292d1ad3b0aa9a34d913bcdb6
GautamMarkande <118103083+GautamMarkande@users.noreply.github.com>
2023-07-10 08:45:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+let counter = document.createElement("p");
+counter.id = "counter";
+counter.innerText = 0;
+let btn = document.createElement("button");
+btn.innerText = "increament";
+btn.id = "incrementBtn";
+document.body.append(counter,btn);
+let count = 0;
+p.innerText = count;
+btn.addEventListener("click", ()=>{
+	count++;
+	p.innerText = count;
+})
